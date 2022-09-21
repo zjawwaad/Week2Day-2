04bfd75d2cd3414a3fb6fe1dd70fa7db93021d58
zjawwaad <zjawwaad@yahoo.com>
2022-09-20 22:05:38 -0400

    Weather commit

---
+++ weather.js
@@ -0,0 +1,10 @@
+
+function loadingweatherreport ( element ){
+    alert ("Loading weather report...");
+}
+
+
+function removecookie (element){
+    let policyContainer = document.querySelector (".cookiewindow");
+    policyContainer.classList.add("hideElement");
+}
